0144bbe9521416b503c3113368de53500f4e36ad
Astarin <berndvertommen@msn.com>
2015-07-29 19:31:07 +0200

    More Setup

---
+++ EvaluationPlatform/EvaluationPlatformWebApi/Controllers/ClassesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EvaluationPlatformDAL.CommandAndQuery;
+using EvaluationPlatformWebApi.Models;
+
+namespace EvaluationPlatformWebApi.Controllers
+{
+    [RoutePrefix("api/class")]
+    public class ClassesController : ApiController
+    {
+        private readonly ICommandProcessor _commandProcessor;
+        private readonly IQueryProccesor _queryProccesor;
+
+        public ClassesController(ICommandProcessor commandProcessor, IQueryProccesor queryProccesor)
+        {
+            _commandProcessor = commandProcessor;
+            _queryProccesor = queryProccesor;
+        }
+
+        [HttpGet]
+        public ClassViewInfo GetClassViewInfo()
+        {
+            return null;
+        }
+    }
+
+
+}
---
+++ EvaluationPlatform/EvaluationPlatformWebApi/Models/ClassViewInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvaluationPlatformDomain.Models;
+
+namespace EvaluationPlatformWebApi.Models
+{
+    public class ClassViewInfo
+    {
+        public string Name { get; set; }
+        public SchoolYear SchoolYear { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
