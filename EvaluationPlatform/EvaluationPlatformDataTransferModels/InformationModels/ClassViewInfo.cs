05cd78ff48688990fea5ba0b83312bacb2bdd783
Bernd Vertommen <berndvertommen@msn.com>
2015-10-15 20:49:09 +0200

    Testing git

--- EvaluationPlatform/EvaluationPlatformDataTransferModels/InformationModels/ClassViewInfo.cs
+++ EvaluationPlatform/EvaluationPlatformDataTransferModels/InformationModels/ClassViewInfo.cs
@@ -4,6 +4,7 @@ namespace EvaluationPlatformDataTransferModels.InformationModels
 {
     public class ClassViewInfo
     {
+        // git test 104
         public string Name { get; set; }
         public SchoolYearInfo SchoolYear { get; set; }
         public ICollection<StudentInfo> Students { get; set; }
