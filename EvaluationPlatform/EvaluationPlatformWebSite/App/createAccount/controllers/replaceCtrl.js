471d207b44a81a5457a0bc56fabbe675173ef00b
MarienRoel <roel.marien@telenet.be>
2015-11-09 17:02:38 +0100

    Added navbar adminTools createAccount app not ready yet

---
+++ EvaluationPlatform/EvaluationPlatformWebSite/App/createAccount/controllers/replaceCtrl.js
@@ -0,0 +1,22 @@
+﻿(function (module) {
+    'use strict';
+
+    function controllerName($scope, $location) {
+        var thiz = this;
+       
+        //Variables
+
+        //private Functions
+        
+        // public functions
+
+        //initiations
+        var init = function () {
+
+        }
+
+        init();
+    }
+
+    module.controller('controllerName', controllerName);
+})(angular.module('app.replace'));
---
+++ EvaluationPlatform/EvaluationPlatformWebSite/App/createAccount/services/replaceService.js
@@ -0,0 +1,22 @@
+﻿(function (module) {
+
+    function serviceName($http, configurationService) {
+        var thiz = this;
+        var baseWebApiUrl = configurationService.baseApiPath;
+        //Variables
+
+        //private Functions
+
+        // public functions
+
+        //initiations
+        var init = function () {
+
+        }
+
+        init();
+
+    }
+
+    module.service('serviceName', serviceName);
+})(angular.module('app.replace'))
