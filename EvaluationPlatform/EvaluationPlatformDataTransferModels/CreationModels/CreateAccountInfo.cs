6ff6ad0d2cea0f061f10d2c72c0e151c5f9a1a65
MarienRoel <roel.marien@telenet.be>
2015-11-30 15:10:25 +0100

    Update account creation form + question in CreateAccountInfo

--- EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateAccountInfo.cs
+++ EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateAccountInfo.cs
@@ -1,8 +1,16 @@
-﻿namespace EvaluationPlatformDataTransferModels.CreationModels
+﻿using EvaluationPlatformDataTransferModels.InformationModels;
+
+
+namespace EvaluationPlatformDataTransferModels.CreationModels
 {
     public class CreateAccountInfo
     {
         // voeg hier all properties toe die nogig zijn om een nieuwe account aan te maken.
+
+        public PersonInfo Person { get; set; }
+        //... wat moet hier nog bij? moet hier niet gewoon een instancsie van de klasse AccountInfo en de klasse account info updaten met alle properties van het domainmodel account?
+        //(of toch de properties die we nodig hebben.)
+
 
     }
 }
