5e6f52f952ea15aa805363a2a253ce3d19084105
MarienRoel <roel.marien@telenet.be>
2015-12-22 18:10:49 +0100

    minor adjustments + createAccountInfo class

--- EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateAccountInfo.cs
+++ EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateAccountInfo.cs
@@ -5,7 +5,7 @@ namespace EvaluationPlatformDataTransferModels.CreationModels
 {
     public class CreateAccountInfo
     {
-        // voeg hier all properties toe die nogig zijn om een nieuwe account aan te maken.
+        // voeg hier all properties toe die nodig zijn om een nieuwe account aan te maken.
         public string Username { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
@@ -14,8 +14,7 @@ namespace EvaluationPlatformDataTransferModels.CreationModels
         public string RoleType { get; set; }
         public PersonInfo Person { get; set; }
         public bool IsTeacher { get; set; }
-        //... wat moet hier nog bij? moet hier niet gewoon een instancsie van de klasse AccountInfo en de klasse account info updaten met alle properties van het domainmodel account?
-        //(of toch de properties die we nodig hebben.)
+       
 
         //!!!!!!! propreties doorgeven met kleine letter beginnen via angular
 
---
+++ EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateCourseInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationPlatformDataTransferModels.CreationModels
+{
+    public class CreateCourseInfo
+    {
+        public string Description { get; set; }
+
+    }
+}
