c4f23c4ab3d43452d7b986378cdb4532348685c6
MarienRoel <roel.marien@telenet.be>
2016-01-20 14:59:30 +0100

    test branch

--- EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateCourseInfo.cs
+++ EvaluationPlatform/EvaluationPlatformDataTransferModels/CreationModels/CreateCourseInfo.cs
@@ -15,6 +15,8 @@ namespace EvaluationPlatformDataTransferModels.CreationModels
         public Guid TeacherID { get; set; }
         //public ScaleInfo Scale { get; set; } nog wel meegeven serverside zo ook studyplans
 
+        //test branch
+
 
     }
 }
