beea8bfd40b9e23d31a396f6617fde941504a794
BerndVertommen <berndvertommen@gmail.com>
2016-02-29 21:28:55 +0100

    Cleaned App.js Moved message config to messageConfig file

--- EvaluationPlatform/EvaluationPlatformWebSite/App/app.js
+++ EvaluationPlatform/EvaluationPlatformWebSite/App/app.js
@@ -1,64 +1,8 @@
-﻿var app = angular.module('app', ['ngRoute', 'toastr', 'ngAnimate', "ui.bootstrap", 'LocalStorageModule', 'app.home', 'app.classes', 'app.login', 'app.account', 'app.index', 'app.student', 'app.evaluationTemplate', 'app.evaluation', 'app.dashboard', 'angular-loading-bar', 'ngTouch', 'app.teacher', 'app.course', 'app.studyPlan'])
-    .config(function ($routeProvider, $httpProvider, toastrConfig) {
-        "use strict";
+﻿var app = angular.module('app',
+    ['ngRoute', 'toastr', 'ngAnimate', "ui.bootstrap", 'LocalStorageModule', 'angular-loading-bar', 'ngTouch'
+    , 'app.home', 'app.classes', 'app.login', 'app.account', 'app.index', 'app.student', 'app.evaluationTemplate', 'app.evaluation', 'app.dashboard'
+    , 'app.teacher', 'app.course', 'app.studyPlan'])
 
-        angular.extend(toastrConfig, {
-            autoDismiss: true,
-            containerId: 'toast-container',
-            maxOpened: 10,
-            newestOnTop: true,
-            positionClass: 'toast-bottom-right',
-            preventDuplicates: false,
-            preventOpenDuplicates: false,
-            target: 'body',
-
-            allowHtml: false,
-            closeButton: false,
-            closeHtml: '<button>&times;</button>',
-            extendedTimeOut: 1000,
-            iconClasses: {
-                error: 'toast-error',
-                info: 'toast-info',
-                success: 'toast-success',
-                warning: 'toast-warning'
-            },
-            messageClass: 'toast-message',
-            onHidden: null,
-            onShown: null,
-            onTap: null,
-            progressBar: false,
-            tapToDismiss: true,
-            templates: {
-                toast: 'directives/toast/toast.html',
-                progressbar: 'directives/progressbar/progressbar.html'
-            },
-            timeOut: 4000,
-            titleClass: 'toast-title',
-            toastClass: 'toast'
-        });
-
-    });
-
-app.config(function ($provide, $httpProvider) {
-    $provide.factory('errorInterceptor', function ($q, $injector) {
-        return {
-            responseError: function (rejection) {
-
-                //console.log(rejection.data.exceptionMessage);
-                
-               //var toastr = $injector.get('toastr');
-               // toastr.error('Fout', rejection.data.exceptionMessage);
-
-                var errorMessageService = $injector.get('messageService');
-                errorMessageService.handleReject(rejection);
-
-                return $q.reject(rejection);
-            }
-        };
-    });
-
-    $httpProvider.interceptors.push('errorInterceptor');
-});
 
 
 
---
+++ EvaluationPlatform/EvaluationPlatformWebSite/App/message/messageConfig.js
@@ -0,0 +1,60 @@
+﻿app.config(function (toastrConfig) {
+    "use strict";
+
+    angular.extend(toastrConfig, {
+        autoDismiss: true,
+        containerId: 'toast-container',
+        maxOpened: 10,
+        newestOnTop: true,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: false,
+        preventOpenDuplicates: false,
+        target: 'body',
+
+        allowHtml: false,
+        closeButton: false,
+        closeHtml: '<button>&times;</button>',
+        extendedTimeOut: 1000,
+        iconClasses: {
+            error: 'toast-error',
+            info: 'toast-info',
+            success: 'toast-success',
+            warning: 'toast-warning'
+        },
+        messageClass: 'toast-message',
+        onHidden: null,
+        onShown: null,
+        onTap: null,
+        progressBar: false,
+        tapToDismiss: true,
+        templates: {
+            toast: 'directives/toast/toast.html',
+            progressbar: 'directives/progressbar/progressbar.html'
+        },
+        timeOut: 4000,
+        titleClass: 'toast-title',
+        toastClass: 'toast'
+    });
+
+});
+
+app.config(function ($provide, $httpProvider) {
+    $provide.factory('errorInterceptor', function ($q, $injector) {
+        return {
+            responseError: function (rejection) {
+
+                //console.log(rejection.data.exceptionMessage);
+
+                //var toastr = $injector.get('toastr');
+                // toastr.error('Fout', rejection.data.exceptionMessage);
+
+                var errorMessageService = $injector.get('messageService');
+                errorMessageService.handleReject(rejection);
+
+                return $q.reject(rejection);
+            }
+        };
+    });
+
+    $httpProvider.interceptors.push('errorInterceptor');
+});
